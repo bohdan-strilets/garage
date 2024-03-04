db1f76fbaca15f9c71f5e6be01ffe10866b3d0ab
Bohdan Strilets <bohdan.strilets@gmail.com>
2024-03-04 12:54:10 +0100

    Installed routes library and create home page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+import { PathsRoute } from "./types/enums/PathsRoute";
+import HomePage from "./pages/HomePage";
+
 const App: React.FC = () => {
-  return <h1>Hello vite</h1>;
+  return (
+    <Routes>
+      <Route path={PathsRoute.Home} element={<HomePage />} />
+    </Routes>
+  );
 };
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./styles/index.css";
 
 const root = document.getElementById("root") as HTMLDivElement;
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
